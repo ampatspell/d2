5de2cead94d236793eb24ad2ac39b49e001f1e8d
Arnis Vuskans <ampatspell@gmail.com>
2025-05-06 21:44:55 +0300

    Update +layout.svelte

--- packages/app/src/routes/(backend)/backend/(regular)/nodes/+layout.svelte
+++ packages/app/src/routes/(backend)/backend/(regular)/nodes/+layout.svelte
@@ -4,7 +4,6 @@
   import type { LayoutData } from './$types';
   import { subscribe } from '$d2/lib/base/model/subscriber.svelte';
   import type { NodeDocumentModel } from '$d2/lib/nodes/node.svelte';
-  import { setGlobal } from '$d2/lib/base/utils/set-global';
   import { isTruthy } from '$d2/lib/base/utils/array';
 
   let { data, children }: { data: LayoutData; children: Snippet } = $props();
@@ -16,9 +15,6 @@
   };
 
   $effect(() => subscribe(nodes));
-  $effect(() => setGlobal({ nodes }));
 </script>
 
-<Nodes {id} {nodes} {route}>
-  {@render children()}
-</Nodes>
+<Nodes {id} {nodes} {route} {children} />
