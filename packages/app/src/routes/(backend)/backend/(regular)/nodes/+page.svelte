955b8567bde03046a72aa5913455aa04632919c8
Arnis Vuskans <ampatspell@gmail.com>
2025-05-09 15:55:00 +0300

    Update +page.svelte

--- packages/app/src/routes/(backend)/backend/(regular)/nodes/+page.svelte
+++ packages/app/src/routes/(backend)/backend/(regular)/nodes/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-  import Add from '$d2/components/dark/section/page/add.svelte';
   import Page from '$d2/components/dark/section/page/page.svelte';
-
-  let onAdd = () => {};
 </script>
 
-{#snippet actions()}
-  <Add {onAdd} />
-{/snippet}
-
-<Page title="Nodes" {actions} />
+<Page title="No selection" />
