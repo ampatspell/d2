f8ef738937a09100c776d62e37aa98bbfc06601e
Arnis Vuskans <ampatspell@gmail.com>
2025-05-13 20:32:12 +0300

    settings/me

---
+++ app/src/d2/components/backend/routes/settings/me/me.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Inspector from '$d2/components/dark/inspector/inspector.svelte';
+  import Section from '$d2/components/dark/inspector/section.svelte';
+  import ValueRow from '$d2/components/dark/inspector/value-row.svelte';
+  import Page from '$d2/components/dark/section/page/page.svelte';
+  import SignOut from '$d2/components/dark/section/page/sign-out.svelte';
+  import { getSession } from '$d2/lib/session/session.svelte';
+
+  let session = getSession();
+  let user = $derived(session.user);
+</script>
+
+{#if user}
+  {#snippet actions()}
+    <SignOut />
+  {/snippet}
+
+  <Page title="Me" {actions}>
+    <Inspector>
+      <Section>
+        <ValueRow label="Email" value={user.email} />
+        <ValueRow label="Role" value={user.role} />
+      </Section>
+    </Inspector>
+  </Page>
+{/if}
--- app/src/routes/(backend)/backend/(regular)/settings/me/+page.svelte
+++ app/src/routes/(backend)/backend/(regular)/settings/me/+page.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-  import SignOut from '$d2/components/dark/section/page/sign-out.svelte';
-  import Page from '$d2/components/dark/section/page/page.svelte';
   import LucideUser from '$d2/icons/lucide--user.svelte';
   import Section from '$d2/components/dark/section/section.svelte';
+  import Me from '$d2/components/backend/routes/settings/me/me.svelte';
 </script>
 
-{#snippet actions()}
-  <SignOut />
-{/snippet}
-
 <Section title="Me" icon={LucideUser}>
-  <Page title="Me" {actions} />
+  <Me />
 </Section>
