5df95129a9263e6252a114b4010f4a2c58ca785a
Arnis Vuskans <ampatspell@gmail.com>
2025-05-14 23:15:56 +0300

    error page

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from '$app/state';
+  import Layout from '$d2/components/layout.svelte';
+  import Dark from '$d2/components/dark/dark.svelte';
+</script>
+
+<Layout>
+  <Dark>
+    <div class="content">
+      {page.status}
+      {page.error?.message}
+    </div>
+  </Dark>
+</Layout>
+
+<style lang="scss">
+  .content {
+    padding: 10px;
+  }
+</style>
