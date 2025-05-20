6ba39d648b12c307cd5e3951fc014c3aa092cd93
Arnis Vuskans <ampatspell@gmail.com>
2025-05-20 22:43:17 +0300

    Update grid.svelte

--- app/src/d2/components/backend/nodes/grid/grid.svelte
+++ app/src/d2/components/backend/nodes/grid/grid.svelte
@@ -11,7 +11,7 @@
   let delegate = options<GridDelegate<NodeModel>>({
     isDraggable: true,
     models: getter(() => models),
-    delegateFor: (node) => {
+    delegateFor: () => {
       return options<GridModelDelegate>({
         isSelected: false,
         select: () => {},
