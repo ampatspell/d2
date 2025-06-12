75146fce880256c55ce54d965f615bf5e29b0c56
Arnis Vuskans <ampatspell@gmail.com>
2025-06-12 15:51:39 +0300

    markdown

---
+++ app/src/routes/(backend)/backend/(regular)/dev/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Page from '$d2/components/dark/section/page/page.svelte';
+    import { toContent } from './models.svelte';
+
+  const thing = async () => {
+    const content = await toContent('# Hello\nworld <foo/>');
+    console.log(content);
+  }
+
+  thing();
+</script>
+
+<Page title="Dev">
+  <div class="page">Hello</div>
+</Page>
+
+<style lang="scss">
+  .page {
+    padding: 10px;
+  }
+</style>
