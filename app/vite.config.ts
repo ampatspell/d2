7f98b1c75fc96daa573290143c68db09424004ff
Arnis Vuskans <ampatspell@gmail.com>
2025-09-04 22:12:02 +0300

    updates

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,12 +1,14 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), devtoolsJson()],
+  plugins: [sveltekit()],
   server: {
     fs: {
-      allow: [searchForWorkspaceRoot(process.cwd()), '../d2'],
-    },
-  },
+      allow: [
+        searchForWorkspaceRoot(process.cwd()),
+        '../d2'
+      ]
+    }
+  }
 });
